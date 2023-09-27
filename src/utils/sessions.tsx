bf8342c88e3fc58e1e20f40810e671dbf3b6425c
fachalik <fachalik@gmail.com>
2023-09-27 21:16:23 +0700

    fix: something

--- src/utils/sessions.tsx
+++ src/utils/sessions.tsx
@@ -1,19 +1,19 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-// import Cookies from 'js-cookie';
+import Cookies from 'js-cookie';
 import { encode, decode } from 'utils/encrypt';
 import { UserType } from '@/models/authModels';
 
 const LOGIN_STORAGE = 'lgstr';
 
 export const setLogin = (state: any) => {
-  localStorage.setItem(LOGIN_STORAGE, encode(state));
+  Cookies.set(LOGIN_STORAGE, encode(state));
 };
 
 export const getLogin = () => {
-  const binary = localStorage.getItem(LOGIN_STORAGE) as string;
+  const binary = Cookies.get(LOGIN_STORAGE) as string;
   return decode(binary);
 };
 
 export const removeLogin = () => {
-  localStorage.removeItem(LOGIN_STORAGE);
+  Cookies.remove(LOGIN_STORAGE);
 };
