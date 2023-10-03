7facfc08f9ee497d0d70cd1f1ca997a7d9b1d816
fachalik <fachalik@gmail.com>
2023-10-03 11:43:09 +0700

    fix: asd

--- src/components/pages/reseller/Dashboard/Chart/SpendingChart.tsx
+++ src/components/pages/reseller/Dashboard/Chart/SpendingChart.tsx
@@ -1,25 +1,29 @@
 'use client';
 
 import React from 'react';
-import { Line } from '@ant-design/charts';
+import dynamic from 'next/dynamic';
+const Line = dynamic(
+  () => import('@ant-design/charts').then(({ Line }) => Line),
+  { ssr: false }
+);
 
 function SpendingChart() {
   const [data, setData] = React.useState([]);
 
-  React.useEffect(() => {
-    asyncFetch();
-  }, []);
+  // React.useEffect(() => {
+  //   asyncFetch();x
+  // }, []);
 
-  const asyncFetch = () => {
-    fetch(
-      'https://gw.alipayobjects.com/os/bmw-prod/55424a73-7cb8-4f79-b60d-3ab627ac5698.json'
-    )
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => {
-        console.log('fetch data failed', error);
-      });
-  };
+  // const asyncFetch = () => {
+  //   fetch(
+  //     'https://gw.alipayobjects.com/os/bmw-prod/55424a73-7cb8-4f79-b60d-3ab627ac5698.json'
+  //   )
+  //     .then((response) => response.json())
+  //     .then((json) => setData(json))
+  //     .catch((error) => {
+  //       console.log('fetch data failed', error);
+  //     });
+  // };
   const config = {
     data,
     xField: 'year',
